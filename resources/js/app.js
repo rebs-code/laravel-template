b4cdf123044b083af8ea604868fe22f05b9428cd
rebs-code <jack.rebussi@gmail.com>
2024-01-26 16:58:13 +0100

    Import Bootstrap JS and fix image import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap';
 // include scss
 import "~resources/scss/app.scss";
+// import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
 //process images
 import.meta.glob(["../img/**'"]);
