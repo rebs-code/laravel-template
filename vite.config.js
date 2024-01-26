e47c4fbe0facc99826c6220de807246549507c8c
rebs-code <jack.rebussi@gmail.com>
2024-01-26 16:55:26 +0100

    Add @popperjs/core and bootstrap dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from "path";
 
 export default defineConfig({
     plugins: [
@@ -12,6 +13,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '~resources': '/resources/',
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
         },
     },
 
